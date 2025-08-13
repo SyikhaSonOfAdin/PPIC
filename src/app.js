96597e58043a9f69f304abdbe49256fc9841e169
SyikhaSonOfAdin <syikhasmart@gmail.com>
2025-08-13 16:21:48 +0700

    [Update v1.4.4] Change version from v1.4.3 to v.1.4.4

--- src/app.js
+++ src/app.js
@@ -24,13 +24,12 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// const corsOptions = {
-//     origin: ['https://ppic.syikha.com', 'https://server1.ppic.syikha.com'],
-//     optionsSuccessStatus: 200
-// };
+const corsOptions = {
+    origin: ['https://ppic.syikha.com', 'https://server1.ppic.syikha.com'],
+    optionsSuccessStatus: 200
+};
 
-// app.use(cors(corsOptions));
-app.use(cors())
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '')));
